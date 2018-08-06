ff20206e541e26eb0bc0b2170bc496ca7652cd37
Albert Tu <albert.t@inwinstack.com>
2018-08-06 16:01:50 +0800

    feat: structure for create queue response

--- pkg/controllers/response.go
+++ pkg/controllers/response.go
@@ -1,9 +1,17 @@
 package controllers
 
 import (
+	"encoding/xml"
+
 	"github.com/gin-gonic/gin"
 	"github.com/minio/minio/cmd"
 )
+
+type CreateQueueResponse struct {
+	XMLName   xml.Name `xml:"CreateQueueResponse"`
+	QueueURL  string   `xml:"CreateQueueResult>QueueUrl"`
+	RequestID string   `xml:"ResponseMetadata>RequestId"`
+}
 
 func writeErrorResponse(c *gin.Context, errorCode cmd.APIErrorCode) {
 	apiError := cmd.GetAPIError(errorCode)
