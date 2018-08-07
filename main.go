3f7f1aad4d396ff97b881fc173160caabdbe41b2
Albert Tu <albert.t@inwinstack.com>
2018-08-07 11:00:25 +0800

    feat: handle GET requests to "/" with DeleteQueue action

--- main.go
+++ main.go
@@ -112,6 +112,8 @@ func main() {
 			controllers.ListQueues(c)
 		case "CreateQueue":
 			controllers.CreateQueue(c)
+		case "DeleteQueue":
+			controllers.DeleteQueue(c)
 		default:
 			reverseProxy()(c)
 		}
