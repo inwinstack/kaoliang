0a5285308100f41e1c0f3fb3d8999a4972468205
Albert Tu <albert.t@inwinstack.com>
2018-08-12 16:49:11 +0800

    test: add test for generate ARN

---
+++ pkg/models/resource_test.go
@@ -0,0 +1,34 @@
+package models_test
+
+import (
+	"testing"
+
+	"gitlab.com/stor-inwinstack/kaoliang/pkg/config"
+	"gitlab.com/stor-inwinstack/kaoliang/pkg/models"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func setup() {
+	config.SetServerConfig()
+}
+
+func TestARN(t *testing.T) {
+	setup()
+
+	Convey("Given a topic instance", t, func() {
+		topic := models.Resource{
+			Service:   models.SNS,
+			AccountID: "tester",
+			Name:      "foobar",
+		}
+
+		Convey("When call ARN method", func() {
+			arn := topic.ARN()
+
+			Convey("Return value should equal to ARN that we defined", func() {
+				So(arn, ShouldEqual, "arn:aws:sns:us-east-1:tester:foobar")
+			})
+		})
+	})
+}
