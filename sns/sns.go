616d3239176a2dc2cb8986e8b43fafde804aef4b
Albert Tu <albert.t@inwinstack.com>
2018-09-05 16:03:34 +0800

    feat: CORS support for SNS

--- sns/sns.go
+++ sns/sns.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
@@ -22,8 +23,22 @@ func init() {
 	models.SetCache()
 }
 
+func setOriginHeader() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Header("Access-Control-Allow-Origin", "*")
+	}
+}
+
 func main() {
 	r := gin.Default()
+	r.Use(setOriginHeader())
+
+	r.OPTIONS("/", func(c *gin.Context) {
+		c.Header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE")
+		c.Header("Access-Control-Allow-Headers", "x-amz-content-sha256,x-amz-date,authorization,host,x-amz-user-agent")
+
+		c.Status(http.StatusNoContent)
+	})
 
 	r.POST("/", func(c *gin.Context) {
 		action := c.PostForm("Action")
