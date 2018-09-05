5f19250eb884808f15ab97627aa89bb78f4040ba
Albert Tu <albert.t@inwinstack.com>
2018-09-05 14:57:32 +0800

    feat: CORS support

--- main.go
+++ main.go
@@ -41,6 +41,8 @@ func init() {
 func main() {
 	r := gin.Default()
 	r.RedirectTrailingSlash = false
+
+	r.OPTIONS("/:bucket", controllers.PreflightRequest)
 	r.GET("/:bucket", controllers.GetBucketNotification)
 	r.PUT("/:bucket", controllers.PutBucketNotification)
 	r.PATCH("/:bucket", controllers.PatchBucketPermission)
