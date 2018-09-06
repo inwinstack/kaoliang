62ff03c556297a87c2bc73d4557bfea0cce69ab7
Vincent <minsheng.l@inwinstack.com>
2018-09-06 06:56:35 +0000

    move nfs related code to ganesha.go and take nfs config from .env

--- pkg/controllers/notification.go
+++ pkg/controllers/notification.go
@@ -7,13 +7,11 @@ import (
 	"errors"
 	"fmt"
 	"io/ioutil"
-	"math/rand"
 	"net/http"
 	"net/http/httputil"
 	"strings"
 	"time"
 
-	"github.com/ceph/go-ceph/rados"
 	"github.com/gin-gonic/gin"
 	"github.com/minio/minio/cmd"
 	"github.com/minio/minio/pkg/event"
@@ -220,7 +218,7 @@ func ReverseProxy() gin.HandlerFunc {
 			case IsAdminUserPath(clientReq.URL.Path) && resp.StatusCode == 200:
 				b, _ := ioutil.ReadAll(resp.Body)
 				resp.Body.Close()
-				go handleNfsExport(clientReq, b)
+				go HandleNfsExport(clientReq, b)
 				resp.Body = ioutil.NopCloser(bytes.NewReader(b)) // put body back for client response
 				return nil
 			case len(clientReq.Header["X-Amz-Copy-Source"]) > 0:
@@ -236,96 +234,5 @@ func ReverseProxy() gin.HandlerFunc {
 
 		proxy := &httputil.ReverseProxy{Director: director, ModifyResponse: modifyResponse}
 		proxy.ServeHTTP(c.Writer, c.Request)
-	}
-}
-
-type RgwUser struct {
-	UserId string   `json:"user_id"`
-	Keys   []RgwKey `json:"keys"`
-}
-
-type RgwKey struct {
-	AccessKey string `json:"access_key"`
-	SecretKey string `json:"secret_key"`
-}
-
-func random(min int, max int) int {
-	rand.Seed(time.Now().Unix())
-	return rand.Intn(max-min) + min
-}
-
-func addNfsExport(body []byte) {
-	// get user info
-	var data RgwUser
-	err := json.Unmarshal(body, &data)
-	if err != nil {
-		return
-	}
-	// only export when create user (same request only add key on second times)
-	if len(data.Keys) > 1 {
-		return
-	}
-	userId := data.UserId
-	accessKey := data.Keys[0].AccessKey
-	secretKey := data.Keys[0].SecretKey
-
-	// connect rados
-	conn, _ := rados.NewConnWithUser("admin")
-	conn.ReadDefaultConfigFile()
-	conn.Connect()
-	defer conn.Shutdown()
-	ioctx, _ := conn.OpenIOContext("nfs-ganesha")
-	defer ioctx.Destroy()
-
-	// check export is not exists
-	exportObjName := fmt.Sprintf("export_%s", userId)
-	// create export obj
-	createNfsExportObj(ioctx, exportObjName, userId, accessKey, secretKey)
-	// add export obj path to export list
-	addExportPathToList(ioctx, "export", "nfs-ganesha", exportObjName)
-}
-
-func addExportPathToList(ioctx *rados.IOContext, exportName string, poolName string, exportObjName string) {
-	append_lock := "export_append_lock"
-	append_cookie := "export_append_cookie"
-	newExport := fmt.Sprintf("%%url \"rados://%s/%s\"\n", poolName, exportObjName)
-	ioctx.LockExclusive(exportName, append_lock, append_cookie, "export_append", 0, nil)
-	ioctx.Append(exportName, []byte(newExport))
-	ioctx.Unlock(exportName, append_lock, append_cookie)
-}
-
-func createNfsExportObj(ioctx *rados.IOContext, exportObjName, userId, accessKey, secretKey string) {
-	exportId := random(1, 65535)
-	exportTemp := `Export {
-	Export_ID = %d;
-	Path = "/";
-	Pseudo = "/%s";
-	Access_Type = RW;
-	Protocols = 4;
-	Transports = TCP;
-	FSAL {
-		Name = RGW; 
-		User_Id = "%s"; 
-		Access_Key_Id ="%s";
-                Secret_Access_Key = "%s";
-        }
-}`
-	export := fmt.Sprintf(exportTemp, exportId, userId, userId, accessKey, secretKey)
-	ioctx.WriteFull(exportObjName, []byte(export))
-}
-
-func handleNfsExport(req *http.Request, body []byte) {
-	_, isSubuser := req.URL.Query()["subuser"]
-	_, isKey := req.URL.Query()["key"]
-	_, isQuota := req.URL.Query()["quota"]
-	_, isCaps := req.URL.Query()["caps"]
-
-	// only handle user related request
-	if isSubuser || isKey || isQuota || isCaps {
-		return
-	}
-	// handle create user
-	if req.Method == "PUT" {
-		addNfsExport(body)
 	}
 }
