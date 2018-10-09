6af7111afc5e4dc998c488a4d22f007b9d693861
Vincent <minsheng.l@inwinstack.com>
2018-10-09 06:05:07 +0000

    rename ganesha.go to nfs.go and add setup permission function

--- pkg/controllers/nfs.go
+++ pkg/controllers/nfs.go
@@ -9,6 +9,8 @@ import (
 	"time"
 
 	"github.com/ceph/go-ceph/rados"
+	"github.com/ceph/go-ceph/rgw"
+	"github.com/minio/minio/cmd"
 	"gitlab.com/stor-inwinstack/kaoliang/pkg/utils"
 )
 
@@ -159,4 +161,96 @@ func HandleNfsExport(req *http.Request, body []byte) {
 		removeNfsExport(uid[0])
 		return
 	}
+}
+
+func extractAccessKey(auth string) string {
+	tokens := strings.Split(auth, " ")
+	if len(tokens) != 2 {
+		return ""
+	}
+	credential := strings.Split(tokens[1], ",")[0]
+	creds := strings.Split(strings.TrimSpace(credential), "=")
+	if len(creds) != 2 {
+		return ""
+	}
+	if creds[0] != "Credential" {
+		return ""
+	}
+	credElements := strings.Split(strings.TrimSpace(creds[1]), "/")
+	if len(credElements) < 5 {
+		return ""
+	}
+	accessKey := strings.Join(credElements[:len(credElements)-4], "/")
+	return accessKey
+}
+
+func setupPermission(parentHandle rgw.RgwFileHandle, path string) {
+	// take current target name
+	index := strings.Index(path, "/")
+	if index == -1 {
+		index = len(path)
+	}
+	target := path[0:index]
+
+	// load target handle
+	handle := parentHandle.Lookup(target)
+	parentAttr := parentHandle.GetAttr()
+	attr := handle.GetAttr()
+
+	// inherit parent's permission if not initailzed
+	if !attr.IsInitialized() {
+		// inherit uid and gid from parent
+		attrMap := map[string]uint{
+			"uid": parentAttr.Uid,
+			"gid": parentAttr.Gid,
+		}
+		// inherit permission when directory only
+		if attr.IsDir() {
+			attrMap["mode"] = (attr.Mode & 0777000) | (parentAttr.Mode & 0777)
+		}
+		handle.SetAttr(attrMap)
+	}
+	//fmt.Println("is dir? ", attr.IsDir())
+	//fmt.Printf("%d %d %o\n", attr.Uid, attr.Gid, attr.Mode)
+
+	// setup child permission
+	if index < len(path) {
+		setupPermission(handle, path[index+1:])
+	}
+	handle.Release()
+}
+
+func InheritNfsPermission(request http.Request) {
+	// check auth
+	auth := request.Header.Get("Authorization")
+	accessKey := extractAccessKey(auth)
+	if accessKey == "" {
+		return
+	}
+	uid, cred, s3Err := cmd.GetCredentials(accessKey)
+	if s3Err != cmd.ErrNone {
+		return
+	}
+
+	//cephUser := utils.GetEnv("NFS_CONFIG_User", "admin")
+	//userArg := fmt.Sprintf("--name=client.%s", cephUser)
+	args := []string{"kaoliang", "--conf=/etc/ceph/ceph.conf", "--name=client.admin", "--cluster=ceph"}
+	radosgw := rgw.Create(args)
+	rgwfs, _ := rgw.Mount(radosgw, uid, cred.AccessKey, cred.SecretKey)
+	defer rgw.Umount(rgwfs)
+
+	path := strings.TrimPrefix(request.URL.Path, "/")
+	index := strings.Index(path, "/")
+
+	if index == -1 { // only bucket in path
+		return
+	}
+
+	bucket := path[0:index]
+	rootHandle := rgw.MakeRgwFileHandle(rgwfs)
+	defer rootHandle.Release()
+	bh := rootHandle.Lookup(bucket)
+	defer bh.Release()
+
+	setupPermission(bh, path[index+1:])
 }
