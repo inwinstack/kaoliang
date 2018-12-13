c45228b70ef7d00a5b8424f393f74adf4413b4e2
Vincent <minsheng.l@inwinstack.com>
2018-12-13 08:38:27 +0000

    flat if statment on mdsearch

--- pkg/controllers/mdsearch.go
+++ pkg/controllers/mdsearch.go
@@ -71,6 +71,19 @@ func escape(s string) (escaped string) {
 	return
 }
 
+func makeInvalidSyntaxResponse(requestID string) ErrorResponse {
+
+	return ErrorResponse{
+		Type: "Sender",
+		Code: "InvalidSyntax",
+		Message: escape("Syntax should be one of following: name==(filename), contenttype==(type), " +
+			"lastmodified(< or <= or > or >=)(duration or YYYY-MM-DDThh:mm)," +
+			"size(<= or < or >= or >)(bytes), etag==(MD5 hash value)"),
+		RequestID: requestID,
+	}
+
+}
+
 func Search(c *gin.Context) {
 	userID, errCode := authenticate(c.Request)
 	if errCode != cmd.ErrNone {
@@ -90,229 +103,232 @@ func Search(c *gin.Context) {
 		return
 	}
 
-	if query := c.Query("query"); query != "" {
-		index := utils.GetEnv("METADATA_INDEX", "")
-		bucket := c.Param("bucket")
-		from, err := strconv.Atoi(c.Query("marker"))
-		if err != nil {
-			from = 0
-		}
-		size, err := strconv.Atoi(c.Query("max-keys"))
-		if err != nil {
-			size = 100
-		}
+	requestID, _ := uuid.NewV4()
+	query := c.Query("query")
 
-		ctx := context.Background()
-		client := models.GetElasticsearch()
-		if client == nil {
-			c.Status(http.StatusGatewayTimeout)
-			return
-		}
+	if query == "" {
+		body := makeInvalidSyntaxResponse(requestID.String())
+		c.JSON(http.StatusBadRequest, body)
+		return
+	}
 
-		boolQuery := elastic.NewBoolQuery()
-		boolQuery = boolQuery.Filter(elastic.NewTermQuery("bucket", bucket))
+	index := utils.GetEnv("METADATA_INDEX", "")
+	from, err := strconv.Atoi(c.Query("marker"))
+	if err != nil {
+		from = 0
+	}
+	size, err := strconv.Atoi(c.Query("max-keys"))
+	if err != nil {
+		size = 100
+	}
 
-		requestID, _ := uuid.NewV4()
-		re := regexp.MustCompile("(name|lastmodified|contenttype|size|etag)(<=|<|==|=|>=|>)(.+)")
-		if group := re.FindStringSubmatch(query); len(group) == 4 {
-			switch group[1] {
-			case "name":
-				if group[2] != "==" {
-					body := ErrorResponse{
-						Type:      "Sender",
-						Code:      "InvalidSyntax",
-						Message:   "Syntax should be name==(filename), the filename is a string and support wildcard character e.g. user_*",
-						RequestID: requestID.String(),
-					}
-					c.JSON(http.StatusBadRequest, body)
-					return
-				}
-				if strings.Contains(group[3], "*") {
-					boolQuery = boolQuery.Must(elastic.NewWildcardQuery("name", group[3]))
-				} else {
-					boolQuery = boolQuery.Must(elastic.NewTermQuery("name", group[3]))
-				}
-			case "contenttype":
-				if group[2] != "==" {
-					body := ErrorResponse{
-						Type:      "Sender",
-						Code:      "InvalidSyntax",
-						Message:   "Syntax should be contenttype==(type), the type is a string and support wildcard character e.g. image/*",
-						RequestID: requestID.String(),
-					}
-					c.JSON(http.StatusBadRequest, body)
-					return
-				}
-				if strings.Contains(group[3], "*") {
-					boolQuery = boolQuery.Must(elastic.NewWildcardQuery("meta.content_type", group[3]))
-				} else {
-					boolQuery = boolQuery.Must(elastic.NewTermQuery("meta.content_type", group[3]))
-				}
-			case "lastmodified":
-				duration := regexp.MustCompile("^[1-9][0-9]*[s|m|h|d|w|M|y]$")
-				matchedDuration := duration.MatchString(group[3])
-				if matchedDuration {
-					switch group[2] {
-					case "<=":
-						boolQuery = boolQuery.Filter(elastic.NewRangeQuery("meta.mtime").Gte(fmt.Sprintf("now-%s", group[3])).Lte("now"))
-					case "<":
-						boolQuery = boolQuery.Filter(elastic.NewRangeQuery("meta.mtime").Gt(fmt.Sprintf("now-%s", group[3])).Lt("now"))
-					case ">=":
-						boolQuery = boolQuery.Filter(elastic.NewRangeQuery("meta.mtime").Lte(fmt.Sprintf("now-%s", group[3])))
-					case ">":
-						boolQuery = boolQuery.Filter(elastic.NewRangeQuery("meta.mtime").Lt(fmt.Sprintf("now-%s", group[3])))
-					default:
-						body := ErrorResponse{
-							Type: "Sender",
-							Code: "InvalidSyntax",
-							Message: escape("Syntax should be lastmodified<=(duration), lastmodified<(duration)," +
-								"lastmodified>=(duration) or lastmodified>(duration)\n\n." +
-								"Duration can accept seconds, minutes, hours, days, weeks, months and years. e.g. 30s, 5m, 6h, 1d, 7w, 3M, 2y."),
-							RequestID: requestID.String(),
-						}
-						c.JSON(http.StatusBadRequest, body)
-						return
-					}
-				}
-				startTime, err := time.Parse("2006-01-02T15:04", group[3])
-				if err == nil {
-					startTimeISO := startTime.Format("2006-01-02T15:04")
-					switch group[2] {
-					case "<=":
-						boolQuery = boolQuery.Filter(elastic.NewRangeQuery("meta.mtime").Lte(fmt.Sprintf("%s", startTimeISO)))
-					case "<":
-						boolQuery = boolQuery.Filter(elastic.NewRangeQuery("meta.mtime").Lt(fmt.Sprintf("%s", startTimeISO)))
-					case ">=":
-						boolQuery = boolQuery.Filter(elastic.NewRangeQuery("meta.mtime").Gte(fmt.Sprintf("%s", startTimeISO)))
-					case ">":
-						boolQuery = boolQuery.Filter(elastic.NewRangeQuery("meta.mtime").Gt(fmt.Sprintf("%s", startTimeISO)))
-					default:
-						body := ErrorResponse{
-							Type: "Sender",
-							Code: "InvalidSyntax",
-							Message: "Syntax should be lastmodified<=(YYYY-MM-DDThh:mm), lastmodified<(YYYY-MM-DDThh:mm)," +
-								"lastmodified>=(YYYY-MM-DDThh:mm) or lastmodified<=(YYYY-MM-DDThh:mm) e.g. 2018-05-26T03:48",
-							RequestID: requestID.String(),
-						}
-						c.JSON(http.StatusBadRequest, body)
-						return
-					}
-				}
+	ctx := context.Background()
+	client := models.GetElasticsearch()
+	if client == nil {
+		c.Status(http.StatusGatewayTimeout)
+		return
+	}
 
-				if !matchedDuration && (startTime == time.Time{}) {
-					body := ErrorResponse{
-						Type: "Sender",
-						Code: "InvalidSyntax",
-						Message: escape("Syntanx should be lastmodified<=(duration or YYYY-MM-DDThh:mm), lastmodified<=(duration or YYYY-MM-DDThh:mm)," +
-							"lastmodified<=(duration or YYYY-MM-DDThh:mm) or lastmodified<=(duration or YYYY-MM-DDThh:mm).\n\n" +
-							"Durations can accept seconds, minutes, hours, days, weeks, months and years. e.g. 30s, 5m, 6h, 1d, 7w, 3m, 2y."),
-						RequestID: requestID.String(),
-					}
-					c.JSON(http.StatusBadRequest, body)
-					return
-				}
-			case "size":
-				size, err := strconv.Atoi(group[3])
-				if err == nil && size >= 0 {
-					switch group[2] {
-					case "<=":
-						boolQuery = boolQuery.Filter(elastic.NewRangeQuery("meta.size").Lte(fmt.Sprintf("%d", size)))
-					case "<":
-						boolQuery = boolQuery.Filter(elastic.NewRangeQuery("meta.size").Lt(fmt.Sprintf("%d", size)))
-					case ">=":
-						boolQuery = boolQuery.Filter(elastic.NewRangeQuery("meta.size").Gte(fmt.Sprintf("%d", size)))
-					case ">":
-						boolQuery = boolQuery.Filter(elastic.NewRangeQuery("meta.size").Gt(fmt.Sprintf("%d", size)))
-					default:
-						body := ErrorResponse{
-							Type: "Sender",
-							Code: "InvalidSyntax",
-							Message: "Syntax should be size<=(bytes), size<(bytes), size>=(bytes) or size>(bytes) " +
-								"and the bytes must be integer and greater than or equal to 0.",
-							RequestID: requestID.String(),
-						}
-						c.JSON(http.StatusBadRequest, body)
-						return
-					}
-				} else {
-					body := ErrorResponse{
-						Type: "Sender",
-						Code: "InvalidSyntax",
-						Message: "Syntax should be size<=(bytes), size<(bytes), size>=(bytes) or size>(bytes) " +
-							"and the bytes must be integer and greater than or equal to 0.",
-						RequestID: requestID.String(),
-					}
-					c.JSON(http.StatusBadRequest, body)
-					return
-				}
-			case "etag":
-				etag := regexp.MustCompile("^[a-f0-9]{32}$")
-				if group[2] == "==" && etag.MatchString(group[3]) {
-					boolQuery = boolQuery.Must(elastic.NewTermQuery("meta.etag", group[3]))
-				} else {
-					body := ErrorResponse{
-						Type:      "Sender",
-						Code:      "InvalidSyntax",
-						Message:   "Syntax should be etag==(MD5 hash value)",
-						RequestID: requestID.String(),
-					}
-					c.JSON(http.StatusBadRequest, body)
-					return
-				}
-			}
-		} else {
+	boolQuery := elastic.NewBoolQuery()
+	boolQuery = boolQuery.Filter(elastic.NewTermQuery("bucket", bucket))
+
+	re := regexp.MustCompile("(name|lastmodified|contenttype|size|etag)(<=|<|==|=|>=|>)(.+)")
+	group := re.FindStringSubmatch(query)
+	if len(group) != 4 {
+		body := makeInvalidSyntaxResponse(requestID.String())
+		c.JSON(http.StatusBadRequest, body)
+		return
+	}
+
+	switch group[1] {
+	case "name":
+		if group[2] != "==" {
 			body := ErrorResponse{
-				Type: "Sender",
-				Code: "InvalidSyntax",
-				Message: escape("Syntax should be one of following: name==(filename), contenttype==(type), " +
-					"lastmodified(< or <= or > or >=)(duration or YYYY-MM-DDThh:mm)," +
-					"size(<= or < or >= or >)(bytes), etag==(MD5 hash value)"),
+				Type:      "Sender",
+				Code:      "InvalidSyntax",
+				Message:   "Syntax should be name==(filename), the filename is a string and support wildcard character e.g. user_*",
 				RequestID: requestID.String(),
 			}
 			c.JSON(http.StatusBadRequest, body)
 			return
 		}
-		searchResult, err := client.Search().
-			Index(index).
-			Query(boolQuery).
-			From(from).
-			Size(size).
-			Pretty(true).
-			Do(ctx)
-
-		if err != nil {
-			panic(err)
+		if strings.Contains(group[3], "*") {
+			boolQuery = boolQuery.Must(elastic.NewWildcardQuery("name", group[3]))
+		} else {
+			boolQuery = boolQuery.Must(elastic.NewTermQuery("name", group[3]))
 		}
-
-		searchResp := SearchResponse{
-			IsTruncated: "false",
+	case "contenttype":
+		if group[2] != "==" {
+			body := ErrorResponse{
+				Type:      "Sender",
+				Code:      "InvalidSyntax",
+				Message:   "Syntax should be contenttype==(type), the type is a string and support wildcard character e.g. image/*",
+				RequestID: requestID.String(),
+			}
+			c.JSON(http.StatusBadRequest, body)
+			return
 		}
-
-		var objs []Object
-		for _, document := range searchResult.Each(reflect.TypeOf(ObjectType{})) {
-			if d, ok := document.(ObjectType); ok {
-				obj := Object{
-					Bucket:         d.Bucket,
-					Key:            d.Name,
-					Instance:       d.Instance,
-					VersionedEpoch: d.VersionedEpoch,
-					LastModified:   d.Meta.Mtime,
-					Size:           d.Meta.Size,
-					Etag:           fmt.Sprintf("\\\"%s\"\\", d.Meta.Etag),
-					ContentType:    d.Meta.ContentType,
-					Owner: struct {
-						ID          string `json:"ID"`
-						DisplayName string `json:"DisplayName"`
-					}{
-						d.Owner.ID,
-						d.Owner.DisplayName,
-					},
+		if strings.Contains(group[3], "*") {
+			boolQuery = boolQuery.Must(elastic.NewWildcardQuery("meta.content_type", group[3]))
+		} else {
+			boolQuery = boolQuery.Must(elastic.NewTermQuery("meta.content_type", group[3]))
+		}
+	case "lastmodified":
+		duration := regexp.MustCompile("^[1-9][0-9]*[s|m|h|d|w|M|y]$")
+		matchedDuration := duration.MatchString(group[3])
+		if matchedDuration {
+			switch group[2] {
+			case "<=":
+				boolQuery = boolQuery.Filter(elastic.NewRangeQuery("meta.mtime").Gte(fmt.Sprintf("now-%s", group[3])).Lte("now"))
+			case "<":
+				boolQuery = boolQuery.Filter(elastic.NewRangeQuery("meta.mtime").Gt(fmt.Sprintf("now-%s", group[3])).Lt("now"))
+			case ">=":
+				boolQuery = boolQuery.Filter(elastic.NewRangeQuery("meta.mtime").Lte(fmt.Sprintf("now-%s", group[3])))
+			case ">":
+				boolQuery = boolQuery.Filter(elastic.NewRangeQuery("meta.mtime").Lt(fmt.Sprintf("now-%s", group[3])))
+			default:
+				body := ErrorResponse{
+					Type: "Sender",
+					Code: "InvalidSyntax",
+					Message: escape("Syntax should be lastmodified<=(duration), lastmodified<(duration)," +
+						"lastmodified>=(duration) or lastmodified>(duration)\n\n." +
+						"Duration can accept seconds, minutes, hours, days, weeks, months and years. e.g. 30s, 5m, 6h, 1d, 7w, 3M, 2y."),
+					RequestID: requestID.String(),
 				}
-				objs = append(objs, obj)
+				c.JSON(http.StatusBadRequest, body)
+				return
+			}
+		}
+		startTime, err := time.Parse("2006-01-02T15:04", group[3])
+		if err == nil {
+			startTimeISO := startTime.Format("2006-01-02T15:04")
+			switch group[2] {
+			case "<=":
+				boolQuery = boolQuery.Filter(elastic.NewRangeQuery("meta.mtime").Lte(fmt.Sprintf("%s", startTimeISO)))
+			case "<":
+				boolQuery = boolQuery.Filter(elastic.NewRangeQuery("meta.mtime").Lt(fmt.Sprintf("%s", startTimeISO)))
+			case ">=":
+				boolQuery = boolQuery.Filter(elastic.NewRangeQuery("meta.mtime").Gte(fmt.Sprintf("%s", startTimeISO)))
+			case ">":
+				boolQuery = boolQuery.Filter(elastic.NewRangeQuery("meta.mtime").Gt(fmt.Sprintf("%s", startTimeISO)))
+			default:
+				body := ErrorResponse{
+					Type: "Sender",
+					Code: "InvalidSyntax",
+					Message: "Syntax should be lastmodified<=(YYYY-MM-DDThh:mm), lastmodified<(YYYY-MM-DDThh:mm)," +
+						"lastmodified>=(YYYY-MM-DDThh:mm) or lastmodified<=(YYYY-MM-DDThh:mm) e.g. 2018-05-26T03:48",
+					RequestID: requestID.String(),
+				}
+				c.JSON(http.StatusBadRequest, body)
+				return
 			}
 		}
 
-		searchResp.Objects = objs
-		c.JSON(http.StatusOK, searchResp)
+		if !matchedDuration && (startTime == time.Time{}) {
+			body := ErrorResponse{
+				Type: "Sender",
+				Code: "InvalidSyntax",
+				Message: escape("Syntanx should be lastmodified<=(duration or YYYY-MM-DDThh:mm), lastmodified<=(duration or YYYY-MM-DDThh:mm)," +
+					"lastmodified<=(duration or YYYY-MM-DDThh:mm) or lastmodified<=(duration or YYYY-MM-DDThh:mm).\n\n" +
+					"Durations can accept seconds, minutes, hours, days, weeks, months and years. e.g. 30s, 5m, 6h, 1d, 7w, 3m, 2y."),
+				RequestID: requestID.String(),
+			}
+			c.JSON(http.StatusBadRequest, body)
+			return
+		}
+	case "size":
+		size, err := strconv.Atoi(group[3])
+		if err == nil && size >= 0 {
+			switch group[2] {
+			case "<=":
+				boolQuery = boolQuery.Filter(elastic.NewRangeQuery("meta.size").Lte(fmt.Sprintf("%d", size)))
+			case "<":
+				boolQuery = boolQuery.Filter(elastic.NewRangeQuery("meta.size").Lt(fmt.Sprintf("%d", size)))
+			case ">=":
+				boolQuery = boolQuery.Filter(elastic.NewRangeQuery("meta.size").Gte(fmt.Sprintf("%d", size)))
+			case ">":
+				boolQuery = boolQuery.Filter(elastic.NewRangeQuery("meta.size").Gt(fmt.Sprintf("%d", size)))
+			default:
+				body := ErrorResponse{
+					Type: "Sender",
+					Code: "InvalidSyntax",
+					Message: "Syntax should be size<=(bytes), size<(bytes), size>=(bytes) or size>(bytes) " +
+						"and the bytes must be integer and greater than or equal to 0.",
+					RequestID: requestID.String(),
+				}
+				c.JSON(http.StatusBadRequest, body)
+				return
+			}
+		} else {
+			body := ErrorResponse{
+				Type: "Sender",
+				Code: "InvalidSyntax",
+				Message: "Syntax should be size<=(bytes), size<(bytes), size>=(bytes) or size>(bytes) " +
+					"and the bytes must be integer and greater than or equal to 0.",
+				RequestID: requestID.String(),
+			}
+			c.JSON(http.StatusBadRequest, body)
+			return
+		}
+	case "etag":
+		etag := regexp.MustCompile("^[a-f0-9]{32}$")
+		if group[2] == "==" && etag.MatchString(group[3]) {
+			boolQuery = boolQuery.Must(elastic.NewTermQuery("meta.etag", group[3]))
+		} else {
+			body := ErrorResponse{
+				Type:      "Sender",
+				Code:      "InvalidSyntax",
+				Message:   "Syntax should be etag==(MD5 hash value)",
+				RequestID: requestID.String(),
+			}
+			c.JSON(http.StatusBadRequest, body)
+			return
+		}
+	default:
+		body := makeInvalidSyntaxResponse(requestID.String())
+		c.JSON(http.StatusBadRequest, body)
+		return
 	}
+	searchResult, err := client.Search().
+		Index(index).
+		Query(boolQuery).
+		From(from).
+		Size(size).
+		Pretty(true).
+		Do(ctx)
+
+	if err != nil {
+		panic(err)
+	}
+
+	searchResp := SearchResponse{
+		IsTruncated: "false",
+	}
+
+	var objs []Object
+	for _, document := range searchResult.Each(reflect.TypeOf(ObjectType{})) {
+		if d, ok := document.(ObjectType); ok {
+			obj := Object{
+				Bucket:         d.Bucket,
+				Key:            d.Name,
+				Instance:       d.Instance,
+				VersionedEpoch: d.VersionedEpoch,
+				LastModified:   d.Meta.Mtime,
+				Size:           d.Meta.Size,
+				Etag:           fmt.Sprintf("\\\"%s\"\\", d.Meta.Etag),
+				ContentType:    d.Meta.ContentType,
+				Owner: struct {
+					ID          string `json:"ID"`
+					DisplayName string `json:"DisplayName"`
+				}{
+					d.Owner.ID,
+					d.Owner.DisplayName,
+				},
+			}
+			objs = append(objs, obj)
+		}
+	}
+
+	searchResp.Objects = objs
+	c.JSON(http.StatusOK, searchResp)
 }
